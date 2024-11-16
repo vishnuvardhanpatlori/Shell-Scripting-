c661c40671b03de259ac230f553775bc7235f4ee
vishnuvardhan0926 <129029564+vishnuvardhanpatlori@users.noreply.github.com>
2024-11-16 22:07:24 +0530

    variable2.sh

---
+++ variable2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#In this script we learm some arthimetic operation in shell scripting language
+num1=10
+num2=20
+num3=30
+
+#sum of the three numbers 
+
+echo "sum = $num1+$num2+$num3"
+
+#substraction of two numbers 
+echo "num = $num3-$num2"
+echo "newnum= $num2-$num1"
