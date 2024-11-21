09b960c4c6db371122842e386ca41b0caa72d8c3
patlori vishnuvardhan <129029564+vishnuvardhanpatlori@users.noreply.github.com>
2024-11-21 21:46:56 +0530

    09 UserInterface.sh

---
+++ 09 UserInterface.sh
@@ -0,0 +1,8 @@
+#In this script we learn about how to read input from user 
+#Syntax
+#1 read Variable_name
+#2 read -p "enter anything " Variable_name
+
+#!/bin/bash
+read name
+echo "your name is $name"
