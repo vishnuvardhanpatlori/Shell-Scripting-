6b36b2d9e64a3e2e6beff431840a716398465bdc
patlori vishnuvardhan <129029564+vishnuvardhanpatlori@users.noreply.github.com>
2024-11-21 22:28:01 +0530

    13 IfElse2.sh

---
+++ 13 IfElse2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#it consist of if else ladder 
+
+read -p "Enter your marks " marks 
+if [ [ $marks -ge 80 ] ]
+then 
+echo "you got FIRST DIVISION "
+elif [ [ $marks -ge 60 ] ]
+then 
+echo "you got 2nd DIVISION  "
+elif [ [ $marks -ge 40 ] ]
+then 
+echo "you got 3rd DIVISION "
+else
+echo "you are FAIL"
+fi
