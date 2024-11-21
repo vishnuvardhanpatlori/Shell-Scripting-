39a866074293e900645938f3c5c196af0ac6fc63
patlori vishnuvardhan <129029564+vishnuvardhanpatlori@users.noreply.github.com>
2024-11-21 22:21:08 +0530

    12 IfElse.sh

---
+++ 12 IfElse.sh
@@ -0,0 +1,20 @@
+# In this we lwarn about IF conditional statment
+#syntax
+# if[ condition ]
+# then
+# statment.......
+# else
+# statment........
+# fi #end of if condition block 
+
+# we use "-gt" to compare  greater and "-ge" for greater than equalto
+# we use "-lt" to compare lower and "-le" for lower than equalto
+
+#!/bin/bash
+read -p "enter your marks:" marks
+if [ [ marks -ge 40 ] ]
+then 
+echo "you are PASSED"
+else
+echo  "you are FAIL"
+fi
