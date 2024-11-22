7f0b9d88071d77eab89c0290a05d24d0db8b848f
patlori vishnuvardhan <129029564+vishnuvardhanpatlori@users.noreply.github.com>
2024-11-22 15:06:43 +0530

    13 IfElse2.sh

--- 13 IfElse2.sh
+++ 13 IfElse2.sh
@@ -2,13 +2,13 @@
 #it consist of if else ladder 
 
 read -p "Enter your marks " marks 
-if [ [ $marks -ge 80 ] ]
+if [ $marks -ge 80 ]
 then 
 echo "you got FIRST DIVISION "
-elif [ [ $marks -ge 60 ] ]
+elif [ $marks -ge 60 ]
 then 
 echo "you got 2nd DIVISION  "
-elif [ [ $marks -ge 40 ] ]
+elif [ $marks -ge 40 ]
 then 
 echo "you got 3rd DIVISION "
 else
