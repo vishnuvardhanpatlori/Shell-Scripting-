9236f45932b0d86d497b1322ce9efb610b82344d
patlori vishnuvardhan <129029564+vishnuvardhanpatlori@users.noreply.github.com>
2025-01-28 21:25:16 +0530

    02_variables1.sh

--- 02_variables1.sh
+++ 02_variables1.sh
@@ -5,14 +5,14 @@ name="vishnu"
 age=20
 occupation="software"
 
-#we use $(doller) symbole to acces the variable 
+#we use $(doller) symbol to acces the variable 
 echo "my nane is $name and my age is $age"
 
 #changes the variable value again 
 name="vishnuvardhan"
 echo "new name is $name and I am a $occupation"
 
-#variable to store output of a commend
+#variable to store output of a command
 HOSTNAME=$(hostname)
 
 echo " new host name is $HOSTNAME"
